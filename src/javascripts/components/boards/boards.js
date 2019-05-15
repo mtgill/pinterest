25198ce6d8398146d75f8541fcd0c4bb109c1fbc
Matt Gill <mtgill90@outlook.com>
2019-05-14 19:39:03 -0500

    buttons printing buttonId to console

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -3,6 +3,20 @@ import util from '../../helpers/util';
 
 let boards = [];
 
+const seePinDiv = (e) => {
+  const boardId = e.target.closest('.card').id;
+  console.error('you clicked a button', boardId);
+  document.getElementById('boards-page').classList.add('hide');
+  document.getElementById('pins-page').classList.add('remove');
+};
+
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-pins');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seePinDiv);
+  }
+};
+
 const domStringBuilder = () => {
   let domString = '';
   boards.forEach((board) => {
@@ -10,11 +24,13 @@ const domStringBuilder = () => {
     domString += `<div id='${board.id}' class="card p-2">`;
     domString += '<div class="card-body">';
     domString += `<h3>${board.name}</h3>`;
+    domString += '<button class="btn btn-warning see-pins">Pins</button>';
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';
   });
   util.printToDom('user-boards', domString);
+  bindEvents();
 };
 
 const initBoards = () => {
