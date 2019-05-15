5313697663ddc23b2f1b310b93df968ec7241147
Matt Gill <mtgill90@outlook.com>
2019-05-14 19:25:11 -0500

    cards printing to dom

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,28 @@
 import boardData from '../../helpers/data/boardsData';
+import util from '../../helpers/util';
+
+let boards = [];
+
+const domStringBuilder = () => {
+  let domString = '';
+  boards.forEach((board) => {
+    domString += '<div class="col-3">';
+    domString += `<div id='${board.id}' class="card p-2">`;
+    domString += '<div class="card-body">';
+    domString += `<h3>${board.name}</h3>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('resp', resp.data.boards);
+      const boardsArray = resp.data.boards;
+      boards = boardsArray;
+      domStringBuilder();
     })
     .catch(err => console.error(err));
 };
